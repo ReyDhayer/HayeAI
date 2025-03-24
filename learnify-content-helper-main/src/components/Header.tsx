273d0969feba5b80575c15978fd5fcafdc1ffdb5
Dhayer <dhayerfort@gmail.com>
2025-03-23 21:47:46 -0300

    Atualizando configurações do Netlify

--- learnify-content-helper-main/src/components/Header.tsx
+++ learnify-content-helper-main/src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
           <ul className="flex space-x-6">
             <li>
               <a 
-                href="src\pages\inicio.html" 
+                href="src/pages/inicio.html" 
                 className="text-foreground/70 hover:text-foreground transition-colors relative after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-primary after:origin-bottom-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-bottom-left"
               >
                 Início
